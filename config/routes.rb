da69f36212ffc73760ddb36a83109cb9ed5ee6e5
sasakure <jyouichirou113377plusultra@gmail.com>
2025-12-20 17:10:26 +0900

    Update stocks index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :stocks
+  
+  # トップページ
   root "home#index"
+
+  # 画面遷移したい先（例）
+  get  "/recipes", to: "recipes#index"
+  get  "/stocks",  to: "stocks#index"
 end
 
